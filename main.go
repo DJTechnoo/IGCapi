0944fd51bff89747a80b2202289bf0a48c8e5935
DJTechnoo <askeltechno@hotmail.com>
2018-10-14 19:20:13 +0200

    formatted code and fized linter warnings

--- main.go
+++ main.go
@@ -1,320 +1,251 @@
 // App made by Askel Eirik Johansson
 // with use of goigc
 
-
-
 package main
 
-
 import (
-
-	"fmt"
-	"time"
 	"encoding/json"
+	"fmt"
+	"github.com/DJTechnoo/goigc"
+	"google.golang.org/appengine"
 	"net/http"
 	"strconv"
 	"strings"
-	"github.com/DJTechnoo/goigc"
-	"google.golang.org/appengine"
-
+	"time"
 )
-
-
 
 // Meta will hold json for metadata /api
 type Meta struct {
-	Uptime string `json:"uptime"`
-	Info string `json:"info"`
+	Uptime  string `json:"uptime"`
+	Info    string `json:"info"`
 	Version string `json:"version"`
 }
 
-
-
 // Fields will hold json for Track
 type Fields struct {
-	H_date time.Time `json:"H_date"`
-	Pilot string `json:"pilot"`
-	Glider string `json:"glider"`
-	Glider_id string `json:"glider_id"`
-	Track_len float64 `json:"track_lenght"`
+	HDate    time.Time `json:"H_date"`
+	Pilot    string    `json:"pilot"`
+	Glider   string    `json:"glider"`
+	GliderID string    `json:"glider_id"`
+	TrackLen float64   `json:"track_lenght"`
 }
-
-
 
 // constants
 
-const ROOT = "/igcinfo"		// this is the root of the app
-const ID_ARG = 4			// URL index for ID
-const FIELD_ARG = 5 		// URL index for FIELD
-const APPTIME = 1539395670	// unix time of deployment
-
+const root = "/igcinfo"    // this is the root of the app
+const idArg = 4            // URL index for ID
+const fieldArg = 5         // URL index for FIELD
+const appTime = 1539395670 // unix time of deployment
 
 // GLOBAL Variables and datastructures
-var lastId int				// Unique last id
-var ids [] string			// slice of ids
-var igcs map[int]string		// urls get associated with ids
-var startTime time.Time		// for UPTIME
-
-
+var lastID int          // Unique last id
+var ids []string        // slice of ids
+var igcs map[int]string // urls get associated with ids
+var startTime time.Time // for UPTIME
 
 //		Serves /igcinfo/api/
 //		Outputs metadata for this app in json
-func metaHandler(w http.ResponseWriter, r * http.Request){
+func metaHandler(w http.ResponseWriter, r *http.Request) {
 
-	meta :=	Meta{
-				Uptime: calculateDuration(time.Since(startTime)),
-				Info: "Api for IGC files",
-				Version: "v0.1"}
-					
+	meta := Meta{
+		Uptime:  calculateDuration(time.Since(startTime)),
+		Info:    "Api for IGC files",
+		Version: "v0.1"}
+
 	m, err := json.MarshalIndent(&meta, "", "    ")
-	if err != nil{
+	if err != nil {
 		fmt.Fprintln(w, err)
 		return
 	}
-	
+
 	fmt.Fprintf(w, string(m))
-	
-	
+
 }
-
-
-
 
 //		Handles arguments passed in the URL
 //		and ID and FIELD and searches through the URL map
 //		to get the Track
 
-func trackJson(trackUrl string , w http.ResponseWriter, r * http.Request){
-		
-		
-		track, err := igc.ParseLocation(trackUrl, r)
-		if err != nil {
-		    status := 404
-		    http.Error(w, http.StatusText(status), status)
-		    return
-		}
-		
-		
-		trackLen := track.Task.Distance()
-		
-		fields := Fields {track.Date, track.Pilot, track.GliderType, track.GliderID, trackLen}
-		m, err := json.MarshalIndent(&fields, "", "    ")
-		if err != nil{
-			fmt.Fprintln(w, err)
-			return
-		}
-	
-		fmt.Fprintf(w, string(m))
+func trackJSON(trackURL string, w http.ResponseWriter, r *http.Request) {
+
+	track, err := igc.ParseLocation(trackURL, r)
+	if err != nil {
+		status := 404
+		http.Error(w, http.StatusText(status), status)
+		return
+	}
+
+	trackLen := track.Task.Distance()
+
+	fields := Fields{track.Date, track.Pilot, track.GliderType, track.GliderID, trackLen}
+	m, err := json.MarshalIndent(&fields, "", "    ")
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	fmt.Fprintf(w, string(m))
 
 }
-
-
-
 
 //	Takes the IGC ID, and Field as arguments
 //	prints on the screen the specified field
 //	of specified ID
-func trackField(index int, field string, w http.ResponseWriter, r * http.Request){
+func trackField(index int, field string, w http.ResponseWriter, r *http.Request) {
 
-		trackUrl := igcs[index]
-		track, err := igc.ParseLocation(trackUrl, r)
-		if err != nil {
-			status := 404
-		    http.Error(w, http.StatusText(status), status)
-		    return
-		}
-		
-		trackLen := track.Task.Distance()
-		
-		switch field {
-			case "pilot": fmt.Fprintln(w, track.Pilot)
-			case "track_length": fmt.Fprintln(w, trackLen)
-			case "glider":	fmt.Fprintln(w, track.GliderType)
-			case "glider_id": fmt.Fprintln(w, track.GliderID)
-			case "H_date": fmt.Fprintln(w, track.Date)
-			default: 	status := 404
-		    			http.Error(w, http.StatusText(status), status)
-		    			return
-		
-		}
+	trackURL := igcs[index]
+	track, err := igc.ParseLocation(trackURL, r)
+	if err != nil {
+		status := 404
+		http.Error(w, http.StatusText(status), status)
+		return
+	}
 
+	trackLen := track.Task.Distance()
+
+	switch field {
+	case "pilot":
+		fmt.Fprintln(w, track.Pilot)
+	case "track_length":
+		fmt.Fprintln(w, trackLen)
+	case "glider":
+		fmt.Fprintln(w, track.GliderType)
+	case "glider_id":
+		fmt.Fprintln(w, track.GliderID)
+	case "H_date":
+		fmt.Fprintln(w, track.Date)
+	default:
+		status := 404
+		http.Error(w, http.StatusText(status), status)
+		return
+
+	}
 
 }
-
-
 
 //	Handles the last two arguments for <ID> and <FIELD>
 //
 //
-func argsHandler(w http.ResponseWriter, r * http.Request){
+func argsHandler(w http.ResponseWriter, r *http.Request) {
 
-	parts := strings.Split(r.URL.Path, "/")				// array of url parts
-	
-	if len(parts) > FIELD_ARG + 1{
+	parts := strings.Split(r.URL.Path, "/") // array of url parts
+
+	if len(parts) > fieldArg+1 {
 		status := 400
 		http.Error(w, http.StatusText(status), status)
 		return
 	}
-	
-	if len(parts) > ID_ARG && len(parts) < FIELD_ARG+1{
-		index, err := strconv.Atoi(parts[ID_ARG])
-		if err != nil{
+
+	if len(parts) > idArg && len(parts) < fieldArg+1 {
+		index, err := strconv.Atoi(parts[idArg])
+		if err != nil {
 			status := 404
-		    http.Error(w, http.StatusText(status), status)
-		    return
+			http.Error(w, http.StatusText(status), status)
+			return
 		}
 		s := igcs[index]
-		trackJson(s, w, r)
-		
+		trackJSON(s, w, r)
+
 	}
-	
-	if len(parts) > FIELD_ARG {
-		index, err := strconv.Atoi(parts[ID_ARG])
-		if err != nil{
+
+	if len(parts) > fieldArg {
+		index, err := strconv.Atoi(parts[idArg])
+		if err != nil {
 			status := 404
-		    http.Error(w, http.StatusText(status), status)
-		    return
+			http.Error(w, http.StatusText(status), status)
+			return
 		}
-		field := string(parts[FIELD_ARG])
-		if index >= 0 && index < lastId {
-			trackField(index, field, w, r)
+
+		if index < 0 || index >= lastID {
+			status := 404
+			http.Error(w, http.StatusText(status), status)
+			return
 		}
-		
-		
+
+		field := string(parts[fieldArg])
+		trackField(index, field, w, r)
+
 	}
 }
-
-
-
 
 //		handles POST and GET
 //		makes use of "form.html" to get a URL to igc file
 //		The URL gets stored with a unique ID in a map
 //		json array outputs list of id's
-func inputHandler(w http.ResponseWriter, r * http.Request){
+func inputHandler(w http.ResponseWriter, r *http.Request) {
 
 	parts := strings.Split(r.URL.Path, "/")
-	
+
 	if len(parts) < 5 {
 		switch r.Method {
-		case "GET":     
-		     http.ServeFile(w, r, "form.html")
+		case "GET":
+			http.ServeFile(w, r, "form.html")
 		case "POST":
-		    
-		    if err := r.ParseForm(); err != nil {
-		        fmt.Fprintf(w, "ParseForm() err: %v", err)
-		        return
-		    }
-		    
-		    
 
-// get url from form and ten see if the url is valid. If it is, store in map
-		    trackUrl := r.FormValue("link")
-		    if _, err := igc.ParseLocation(trackUrl, r); err != nil {
-		    	status := 400
+			if err := r.ParseForm(); err != nil {
+				fmt.Fprintf(w, "ParseForm() err: %v", err)
+				return
+			}
+
+			// get url from form and ten see if the url is valid. If it is, store in map
+			trackURL := r.FormValue("link")
+			if _, err := igc.ParseLocation(trackURL, r); err != nil {
+				status := 400
 				http.Error(w, http.StatusText(status), status)
 				return
-		    }
-		    
-		    igcs[lastId] = string(trackUrl)
-		    idManager(w);
-		    
+			}
+
+			igcs[lastID] = string(trackURL)
+			idManager(w)
+
 		default:
-		    fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+			fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
 		}
-    } else {
-    	fmt.Fprintln(w, "More params!")
-    }
+	} else {
+		fmt.Fprintln(w, "More params!")
+	}
 }
-
-
-
 
 //	Creates a new ID for the next url
 //	and appends to ID-slice.
 //
-func idManager(w http.ResponseWriter){
-	ids = append(ids, strconv.Itoa(lastId))
-    lastId++
-    
-    idsJson, _ := json.MarshalIndent(ids, "", "    ")
-    fmt.Fprintln(w, string(idsJson))
-    
+func idManager(w http.ResponseWriter) {
+	ids = append(ids, strconv.Itoa(lastID))
+	lastID++
+
+	idsJSON, _ := json.MarshalIndent(ids, "", "    ")
+	fmt.Fprintln(w, string(idsJSON))
+
 }
-
-
 
 //	Input: Time in seconds
 //	Output: string of ISO 8601 of said time
 //
-func calculateDuration(t time.Duration)(string){
+func calculateDuration(t time.Duration) string {
 	startTime = time.Now()
-	totalTime := int(startTime.Unix()) - APPTIME //int(t) / int(time.Second)
+	totalTime := int(startTime.Unix()) - appTime //int(t) / int(time.Second)
 
-	remainderSeconds 	:= totalTime%60				// final seconds
-	minutes				:= totalTime / 60
-	remainderMinutes	:= minutes%60					// final minutes
-	hours				:= minutes / 60
-	remainderHours		:= hours%24					// final hours
-	days				:= hours / 24
-	remainderDays		:= days%7						// final days
-	months				:= days / 28
-	remainderMonths		:= months%12 					// final months
-	years				:= months / 12		// final years
+	remainderSeconds := totalTime % 60 // final seconds
+	minutes := totalTime / 60
+	remainderMinutes := minutes % 60 // final minutes
+	hours := minutes / 60
+	remainderHours := hours % 24 // final hours
+	days := hours / 24
+	remainderDays := days % 7 // final days
+	months := days / 28
+	remainderMonths := months % 12 // final months
+	years := months / 12           // final years
 
-	
-	s := "P"+strconv.Itoa(years)+"Y"+strconv.Itoa(remainderMonths)+"M"+strconv.Itoa(remainderDays)+"D"+strconv.Itoa(remainderHours)+"H"+strconv.Itoa(remainderMinutes)+"M"+strconv.Itoa(remainderSeconds)+"S"
-	return s	
+	s := "P" + strconv.Itoa(years) + "Y" + strconv.Itoa(remainderMonths) + "M" + strconv.Itoa(remainderDays) + "D" + strconv.Itoa(remainderHours) + "H" + strconv.Itoa(remainderMinutes) + "M" + strconv.Itoa(remainderSeconds) + "S"
+	return s
 }
 
-
-
-
-func main(){
+func main() {
 	startTime = time.Now()
 	igcs = make(map[int]string)
-	http.HandleFunc(ROOT + "/api", metaHandler);
-	http.HandleFunc(ROOT + "/api/igc", inputHandler);
-	http.HandleFunc(ROOT + "/api/igc/", argsHandler);
+	http.HandleFunc(root+"/api", metaHandler)
+	http.HandleFunc(root+"/api/igc", inputHandler)
+	http.HandleFunc(root+"/api/igc/", argsHandler)
 	appengine.Main()
 
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
